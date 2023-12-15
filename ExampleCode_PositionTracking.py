274d3a958fbb140dba3e940c13e7e1e8517c82eb
ElliottB4 <106140514+ElliottB4@users.noreply.github.com>
2023-12-15 14:47:39 -0800

    Update ExampleCode_PositionTracking.py
    Removed imports because it's kind of unnecessary

--- ExampleCode_PositionTracking.py
+++ ExampleCode_PositionTracking.py
@@ -6,15 +6,13 @@ Created on Fri Dec 15 14:41:01 2023
 @author: elliottbryniarski
 """
 
-from math import cos
-
 x, y, z = bno.getVector(BNO055.VECTOR_EULER)
 
 ticks1 = time.ticks_us()
 
 V = ENCL.get_velocity()
 
-delta_t = ticksdiff(ticks1,ticks2)
+delta_t = ticks_diff(ticks1,ticks2)
 
 X_pos = V * delta_t * cos(x) + X_pos
 
